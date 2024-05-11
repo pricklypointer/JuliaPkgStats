159ded2507d3b1dbea4cad04255f7b854bb07c29
pricklypointer <pricklypointer@gmail.com>
2024-05-11 14:11:51 -0700

    Add initial trending package table

--- TopPackageRequests.jl
+++ TopPackageRequests.jl
@@ -8,7 +8,8 @@ using LibPQ
 include("layout_shared.jl")
 include("utils.jl")
 
-const N_PACKAGES = 100
+const N_PACKAGES_TOP = 100
+const N_PACKAGES_TRENDING = 10
 
 function get_request_count(
     conn, time_interval, limit; user_data=true, ci_data=false, missing_data=false
@@ -33,6 +34,53 @@ function get_request_count(
     return df
 end
 
+function get_trending_requests(
+    conn, limit; user_data=true, ci_data=false, missing_data=false, daily_minimum=5
+)
+    conditions = generate_sql_conditions(user_data, ci_data, missing_data)
+    sql = """
+    with max_date AS(
+        SELECT MAX(date) AS max_date
+        FROM juliapkgstats.package_requests_by_date
+    ),
+    last_period_requests AS (
+        SELECT package_name, SUM(request_count) AS total_requests, count(date) as n_days
+        FROM juliapkgstats.package_requests_by_date
+        INNER JOIN juliapkgstats.uuid_name ON juliapkgstats.package_requests_by_date.package_id = juliapkgstats.uuid_name.package_id
+        CROSS JOIN max_date
+        WHERE (date BETWEEN max_date - INTERVAL '14 days' and max_date) $conditions
+            AND request_count > $daily_minimum
+            AND package_name not like '%_jll'
+        GROUP BY package_name
+        HAVING count(date) > 12
+        ORDER BY total_requests DESC
+    ),
+    previous_period_requests AS (
+        SELECT package_name, SUM(request_count) AS total_requests
+        FROM juliapkgstats.package_requests_by_date
+        INNER JOIN juliapkgstats.uuid_name ON juliapkgstats.package_requests_by_date.package_id = juliapkgstats.uuid_name.package_id
+        CROSS JOIN max_date
+        WHERE (date BETWEEN max_date - INTERVAL '27 days' and max_date - INTERVAL '14 days') $conditions
+            AND request_count > $daily_minimum
+            AND package_name not like '%_jll'
+        GROUP BY package_name
+        HAVING count(date) > 12
+        ORDER BY total_requests DESC
+    )
+    select lpr.package_name,
+            ROUND(100.0 * (lpr.total_requests - ppr.total_requests) / ppr.total_requests, 2) AS percent_change
+    from last_period_requests lpr
+    INNER JOIN previous_period_requests ppr
+        ON lpr.package_name = ppr.package_name
+    WHERE lpr.total_requests > ppr.total_requests
+    ORDER BY percent_change DESC
+    limit $limit
+    """
+    df = DataFrame(LibPQ.execute(conn, sql))
+    df[!, :percent_change] = ["$i%" for i in df.percent_change]
+    return df
+end
+
 function ui()
     [   
         layout_shared()...,
@@ -45,16 +93,23 @@ function ui()
         ]),
         cell(class="row", [
             cell(class="st-col col-12 col-sm st-module", [
-                h4("Most downloaded past month."),
+                h4("Top This Month"),
                 GenieFramework.table(:monthly_requests; pagination=DataTablePagination(rows_per_page=20), dense=true, flat=true)
             ]),
             cell(class="st-col col-12 col-sm st-module", [
-                h4("Most downloaded past week."),
+                h4("Top This Week"),
                 GenieFramework.table(:weekly_requests; pagination=DataTablePagination(rows_per_page=20), dense=true, flat=true)
             ]),
             cell(class="st-col col-12 col-sm st-module", [
-                h4("Most downloaded past day."),
+                h4("Top Yesterday"),
                 GenieFramework.table(:daily_requests; pagination=DataTablePagination(rows_per_page=20), dense=true, flat=true)
+            ]),
+        ]),
+        hr(style="border: 1px solid #ccc; margin: 20px 0;"),
+        cell(class="row", [
+            cell(class="st-col col-12 col-sm st-module", [
+                h4("Trending Downloads (last 2 weeks)"),
+                GenieFramework.table(:trending_requests; pagination=DataTablePagination(rows_per_page=10), dense=true, flat=true)
             ]),
         ]),
         footer("The figures above represent unique IP address download counts for each package. The count might not fully reflect the actual downloads for packages with less frequent releases, and multiple counts could occur for users with dynamic IP addresses.", href="https://discourse.julialang.org/t/announcing-package-download-stats/69073")
@@ -65,6 +120,7 @@ end
     monthly_requests::DataTable = DataTable()
     weekly_requests::DataTable = DataTable()
     daily_requests::DataTable = DataTable()
+    trending_requests::DataTable = DataTable()
 end
 
 @app begin
@@ -75,6 +131,7 @@ end
     @out monthly_requests = DataTable()
     @out weekly_requests = DataTable()
     @out daily_requests = DataTable()
+    @out trending_requests = DataTable()
 
     @onchange isready begin
         @push
@@ -84,13 +141,16 @@ end
         @info "Change in checkboxes on AllPackageRequests (user_data: $user_data, ci_data: $ci_data, missing_data: $missing_data)"
         conn = LibPQ.Connection(conn_str)
         monthly_requests = DataTable(get_request_count(
-            conn, "1 month", N_PACKAGES; user_data, ci_data, missing_data
+            conn, "1 month", N_PACKAGES_TOP; user_data, ci_data, missing_data
         ))
         weekly_requests = DataTable(get_request_count(
-            conn, "7 day", N_PACKAGES; user_data, ci_data, missing_data
+            conn, "7 day", N_PACKAGES_TOP; user_data, ci_data, missing_data
         ))
         daily_requests = DataTable(get_request_count(
-            conn, "1 day", N_PACKAGES; user_data, ci_data, missing_data
+            conn, "1 day", N_PACKAGES_TOP; user_data, ci_data, missing_data
+        ))
+        trending_requests = DataTable(get_trending_requests(
+            conn, N_PACKAGES_TRENDING; user_data, ci_data, missing_data
         ))
         close(conn)
     end
@@ -98,14 +158,16 @@ end
 
 route("/top"; method=GET) do
     conn = LibPQ.Connection(conn_str)
-    monthly_requests = DataTable(get_request_count(conn, "1 month", N_PACKAGES))
-    weekly_requests = DataTable(get_request_count(conn, "7 day", N_PACKAGES))
-    daily_requests = DataTable(get_request_count(conn, "1 day", N_PACKAGES))
+    monthly_requests = DataTable(get_request_count(conn, "1 month", N_PACKAGES_TOP))
+    weekly_requests = DataTable(get_request_count(conn, "7 day", N_PACKAGES_TOP))
+    daily_requests = DataTable(get_request_count(conn, "1 day", N_PACKAGES_TOP))
+    trending_requests = DataTable(get_trending_requests(conn, N_PACKAGES_TRENDING))
     close(conn)
     model = @init
     model.monthly_requests[] = monthly_requests
     model.weekly_requests[] = weekly_requests
     model.daily_requests[] = daily_requests
+    model.trending_requests[] = trending_requests
     html(page(model, ui()))
 end
 
