99e4e0452e65853262cb2dd814a2a1603c8c7921
pricklypointer <pricklypointer@gmail.com>
2024-05-23 21:26:42 -0700

    Fix data to pull in 301, 302 status codes. Add precompile

---
+++ precompile.jl
@@ -0,0 +1,9 @@
+using HTTP
+
+HTTP.get("https://juliapkgstats.com")
+HTTP.get("https://juliapkgstats.com/all")
+HTTP.get("https://juliapkgstats.com/api")
+HTTP.get("https://juliapkgstats.com/top")
+HTTP.get("https://juliapkgstats.com/pkg/Tidier")
+HTTP.get("https://juliapkgstats.com/api/v1/monthly_downloads/Tidier")
+HTTP.get("https://juliapkgstats.com/api/v1/total_downloads/Tidier")
